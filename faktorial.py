30fa66456b4fdb167d9a8aaf21bf614f1eb83f92
ruslan6265 <46610804+ruslan6265@users.noreply.github.com>
2019-06-29 18:21:44 +0400

    Add files via upload

---
+++ faktorial.py
@@ -0,0 +1,75 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": 1,
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "import sys"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 3,
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "def fakt(a):\n",
+    "    i=1\n",
+    "    for j in range(1,a+1):\n",
+    "        i*=j\n",
+    "    return  i"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 4,
+   "metadata": {},
+   "outputs": [
+    {
+     "name": "stderr",
+     "output_type": "stream",
+     "text": [
+      "Argument xetasi"
+     ]
+    }
+   ],
+   "source": [
+    "if len(sys.argv)==2:\n",
+    "    print(fakt(sys.argv[1]))\n",
+    "else:\n",
+    "    sys.stderr.write(\"Argument xetasi\")\n",
+    "    sys.stderr.flush()"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.6.7"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 2
+}
